b17546567356cc9311577eb1ceddd74cc5afbc10
Haris Kovac <hariskovac19@gmail.com>
2022-07-23 20:13:02 -0500

    Complete sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let sum = 0;
+  let start = num1;
+  let end = num2;
 
+  if (num1 > num2) {
+    start = num2;
+    end = num1;
+  } else if (num1 < 0 || num2 < 0 || typeof num1 != "number" || typeof num2 != "number") {
+    return "ERROR";
+  }
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
