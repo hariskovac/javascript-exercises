9a71fa29728ed357d70479be43a3e66912482b02
Haris Kovac <hariskovac19@gmail.com>
2022-07-23 19:49:05 -0500

    Complete removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+  let newArr = [];
 
+  outer: for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arguments.length; j++) {
+      if (arr[i] === arguments[j]) {
+        continue outer;
+      }
+    }
+    newArr.push(arr[i]);
+  }
+  return newArr;
 };
 
 // Do not edit below this line
