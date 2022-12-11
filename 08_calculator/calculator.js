421aa2bc76849bf8397e7b97ac9086ba2bf6b17e
Haris Kovac <hariskovac19@gmail.com>
2022-12-11 17:16:52 -0600

    Complete 08_calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(
+    (total, current) => total += current, 0
+    );
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce(
+    (product, num) => product *= num, 1
+  )
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(nums) {
+	if (nums === 0) {
+    return 1;
+  } else {
+    return nums * (factorial(nums - 1));
+  }
 };
 
 // Do not edit below this line
