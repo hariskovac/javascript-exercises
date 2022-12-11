168ffb56eef7965bc69134070e55453904976b25
Haris Kovac <hariskovac19@gmail.com>
2022-12-11 17:57:53 -0600

    Complete 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function(str) {
+  const newStr = str
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s/g, "");
+  const reversedStr = newStr
+    .split("")
+    .reverse()
+    .join("");
+  if (newStr.toLowerCase() === reversedStr.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
