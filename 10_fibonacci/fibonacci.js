1621dbee6f7208f10bc64054b5fd9353d71fd478
Haris Kovac <hariskovac19@gmail.com>
2022-12-11 20:21:04 -0600

    Complete 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+  const place = parseInt(member);
+  if (place < 0) {
+    return "OOPS";
+  } else {
+    let fibArr = [0, 1];
+    for (let i = 1; i < place; i++) {
+      fibArr.push(fibArr[i-1] + fibArr[i]);
+      console.log(fibArr);
+    }
+    return fibArr[fibArr.length - 1];
+  }
 };
 
 // Do not edit below this line
